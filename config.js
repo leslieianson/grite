9e425b0f2b247dd659e7efb2ea2000382b4be41b
Leslie I'Anson <leslieianson@users.noreply.github.com>
2014-02-08 14:46:40 +0000

    first config

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 var config = {
-  title: 'Grite',
+  title: 'My First Grite Blog',
   navbar: [ { text: 'Fork me on Github',
               link: 'https://github.com/sankha93/grite' },
             { text: 'Documentation',
               link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
-  posts: [ '5711471',
-           '5711492'],
+  posts: [ '8884730' ],
   atOnce: 5,
-  footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
+  footerText: "&copy; 2014 Leslie I'Anson"
 };
